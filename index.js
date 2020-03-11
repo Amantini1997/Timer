eba38bc4a3020e1c9479275d117799343d728e53
Alessandro Amantini <ale.amantini1997@gmail.com>
2020-03-11 14:40:32 +0000

    Separate html file into js and css

---
+++ index.js
@@ -0,0 +1,63 @@
+function playAudio(audioID) {
+    document.getElementById(audioID).play()
+}
+
+function startSound() {
+    playAudio("startSound");
+}
+
+function stopSound() {
+    playAudio("stopSound");
+}
+
+function endSound() {
+    playAudio("endSound");
+}
+
+function printTime(e = null, msTime){
+    e.innerHtml = `${msTime}s`;
+}
+
+async function rest(timer, msTime, times){
+    if(times < 0){
+        timer.innerHTML = "DONE!";
+        document.querySelector("#start").disabled = false;
+        return;
+    }
+    setTimeout(() => timer.classList.add("rest"), 1000);
+    let copyTime = msTime;
+    timerInterval = await setInterval(() => {
+        timer.innerHTML = `${copyTime*0.001}s`;
+        copyTime -= 1000;
+        if(copyTime < 0) {                    
+            startSound();
+            clearInterval(timerInterval);
+            act(timer, msTime, times);
+        }
+    },1000);
+}
+
+async function act(timer, msTime, times){
+    document.getElementById("remaining").innerHTML = times;
+    setTimeout(() => timer.classList.remove("rest"), 1000);
+    let copyTime = msTime;
+    timerInterval = await setInterval(() => {
+        timer.innerHTML = `${copyTime*0.001}s`;
+        copyTime -= 1000;
+        if(copyTime < 0){
+            times === 0 ? endSound() : stopSound();
+            clearInterval(timerInterval);
+            rest(timer, msTime, --times);
+        }
+    },1000);
+}
+
+async function start(button){
+    button.disabled = true;
+    let time = +document.getElementById("time").value;
+    time *= 1000;
+    let times = +document.getElementById("times").value - 1;
+    let timer = document.querySelector("#timer");
+    timer.classList.remove("disabled");
+    await act(timer, time, times);
+}
