177f4612325b40c28f630e29add554195acadb14
Alessandro Amantini <ale.amantini1997@gmail.com>
2020-03-12 08:41:56 +0000

    modify service worker

--- sw.js
+++ sw.js
@@ -1,15 +1,3 @@
-self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => {
-            // Cache hit - return response
-            return response || fetch(event.request);
-        }
-      )
-    );
-});
-
-
 var CACHE_NAME = 'timer-cache';
 var urlsToCache = [
   '/',
@@ -26,5 +14,16 @@ self.addEventListener('install', event => {
           console.log('Opened cache');
           return cache.addAll(urlsToCache);
       })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+          // Cache hit - return response
+          return response || fetch(event.request);
+      }
+    )
   );
 });
