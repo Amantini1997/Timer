33d5341735ac63758386991727e9e47ed33601d4
Amantini1997 <ale.amantini1997@gmail.com>
2021-11-21 17:14:18 +0100

    test commit for offline mode

--- sw.js
+++ sw.js
@@ -1,19 +1,24 @@
-const APP_IS_DEPLOYED_ON_GITHUB_PAGES = true;
+const APP_IS_DEPLOYED_ON_GITHUB_PAGES = false;
 
 const CACHE_NAME = 'timer-cache';
 const URLS_TO_CACHE = [
   '',
   'index.html',
+
   'styles/index.css',
+
   'scripts/index.js',
   'scripts/screenLocker.js',
   'scripts/sounds.js',
+  'scripts/pwa.js',
+
   'sounds/start.ogg',
   'sounds/stop.ogg',
   'sounds/end.ogg',
+
   'images/192x192.png',
   'images/512x512.png'
-].map(path => (APP_IS_DEPLOYED_ON_GITHUB_PAGES ? "" : "./") + path);
+].map(path => (APP_IS_DEPLOYED_ON_GITHUB_PAGES ? "/" : "./") + path);
 
 // Add RxJs CDN
 URLS_TO_CACHE.push('https://cdnjs.cloudflare.com/ajax/libs/rxjs/6.1.0/rxjs.umd.js');
@@ -22,12 +27,6 @@ URLS_TO_CACHE.push('https://cdnjs.cloudflare.com/ajax/libs/rxjs/6.1.0/rxjs.umd.j
 self.addEventListener('install', event => {
   console.log('[Service Worker] Install');
   event.waitUntil(loadCache());
-  // Perform install steps
-  // event.waitUntil((async () => {
-  //   const cache = await caches.open(CACHE_NAME);
-  //   console.log('[Service Worker] Caching all: app shell and content');
-  //   await cache.addAll(URLS_TO_CACHE);
-  // })());
 });
 
 async function loadCache() {
@@ -38,6 +37,9 @@ async function loadCache() {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(eventReq => eventReq || fetch(event.request))
+    caches.match(event.request).then(eventReq => {
+      console.log("request is", eventReq?.url, "or", event.request.url)
+      return eventReq || fetch(event.request)
+    })
   );
 });
